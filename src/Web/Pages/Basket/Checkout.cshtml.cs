bd3479924bac7e3a390dec662e4a6dc819fd9630
Carlos Bonilla <cab989@gmail.com>
2026-05-23 14:54:45 -0500

    Possible null value issue for azureFunction string value was solved.

--- src/Web/Pages/Basket/Checkout.cshtml.cs
+++ src/Web/Pages/Basket/Checkout.cshtml.cs
@@ -56,9 +56,11 @@ public class CheckoutModel : PageModel
                 return BadRequest();
             }
 
+            var azureFunction = _configuration["AzureFunction:CreateOrderUrl"] ?? ""; 
+
             var updateModel = items.ToDictionary(b => b.Id.ToString(), b => b.Quantity);
             await _basketService.SetQuantities(BasketModel.Id, updateModel);
-            await _orderService.CreateOrderAsync(BasketModel.Id, new Address("123 Main St.", "Kent", "OH", "United States", "44240"), _configuration["AzureFunction:CreateOrderUrl"]);
+            await _orderService.CreateOrderAsync(BasketModel.Id, new Address("123 Main St.", "Kent", "OH", "United States", "44240"), azureFunction);
             await _basketService.DeleteBasketAsync(BasketModel.Id);
         }
         catch (EmptyBasketOnCheckoutException emptyBasketOnCheckoutException)
